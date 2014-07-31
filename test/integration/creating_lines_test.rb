125b1fdee401089c55e1a9859385e77983ce7e91
eldano <eldano.v2@gmail.com>
2014-07-31 17:38:19 -0300

    test that line gets created

--- test/integration/creating_lines_test.rb
+++ test/integration/creating_lines_test.rb
@@ -5,8 +5,7 @@ class CreatingLinesTest < ActionDispatch::IntegrationTest
 	setup { host! 'api.lines.com' }
 
 	test 'creates line' do
-		line = Line.create!(name: "First line")
-
+		
 		post '/lines', 
 			{ name: "new line" }.to_json,
 			{ 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
@@ -15,6 +14,8 @@ class CreatingLinesTest < ActionDispatch::IntegrationTest
 		
 		json = json(response.body)
 		assert_equal api_line_url(json[:line][:id]), response.location
+
+		assert_equal "new line", Line.find(json[:line][:id]).name
 	end
 
 end
